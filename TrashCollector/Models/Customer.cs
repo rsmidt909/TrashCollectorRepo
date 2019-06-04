b67827bb9a5467b4e8f36294c0bfc5a1cca38759
Robert Smidt <rsmidt909@hotmail.com>
2019-06-04 14:58:38 -0500

    Created customer model

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        public string Firstname { get; set; }
+        public int Lastname { get; set; }
+        public string StreetAddress { get; set; }
+        public int ZipCode { get; set; }
+        public string SpecificPickUpDate { get; set; }
+        public string FreezeTimeFrame { get; set; }
+    }
+}
